89ffa5b3e004c9f050c4bcf171f325c9d07ffd49
Chintogtokh Batbold <bchintogtokh@gmail.com>
2022-12-03 15:22:11 +1100

    merging

--- src/projects/linkToWikidata/linkToWikidata.ts
+++ src/projects/linkToWikidata/linkToWikidata.ts
@@ -1,15 +1,13 @@
 import { ApiParams, mwn } from "mwn";
 import { WikidataBotConfig } from "../../utils/bot";
+import { getWikidataIds } from "./linkToWikidataInteractive";
 
 const linkToWikidata = async () => {
   const bot = new mwn(WikidataBotConfig());
   await bot.login();
 
   // mn, en
-  const linksneeded = [
-    ["Ангилал:1580 он", "Category:1580"],
-    ["Ангилал:1576 он", "Category:1576"],
-  ];
+  const linksneeded = [["Бизнесийн оюун ухаан", "Business intelligence"]];
 
   for await (const item of linksneeded) {
     await bot.getTokens();
@@ -18,20 +16,35 @@ const linkToWikidata = async () => {
 
     const token = await bot.getCsrfToken();
 
+    const mnArticle = item[0];
+    const enArticle = item[1];
+
     const params: ApiParams = {
       action: "wblinktitles",
       fromsite: "mnwiki",
-      fromtitle: item[0],
+      fromtitle: mnArticle,
       tosite: "enwiki",
-      totitle: item[1],
+      totitle: enArticle,
       token,
     };
 
-    const result = await bot
-      .query(params)
-      .catch((e) => console.log("Error occured: ", e));
+    const result = await bot.query(params).catch(async (e: any) => {
+      console.log("Error occured, try merging: ", e);
+      const wikiDataIds = await getWikidataIds(bot, mnArticle, enArticle);
+      if (wikiDataIds) {
+        const mergeParams: ApiParams = {
+          action: "wbmergeitems",
+          fromid: wikiDataIds.mn,
+          toid: wikiDataIds.en,
+          token: token,
+        };
 
-    console.log(result);
+        const nextResult = await bot.query(mergeParams);
+        console.log(nextResult);
+      } else {
+        console.log("Not mergeable");
+      }
+    });
 
     await new Promise((r) => setTimeout(r, 1500));
   }
--- src/projects/linkToWikidata/linkToWikidataInteractive.ts
+++ src/projects/linkToWikidata/linkToWikidataInteractive.ts
@@ -2,6 +2,56 @@ import { ApiParams, mwn } from "mwn";
 import { WikidataBotConfig } from "../../utils/bot";
 import * as readline from "readline";
 
+type WikibaseEntity = {
+  entities: {
+    [key: string]: {
+      pageid: number;
+      title: string;
+      sitelinks: any;
+    };
+  };
+  success: boolean;
+};
+
+export const getWikidataIds = async (
+  bot: mwn,
+  mnArticle: string,
+  enArticle: string
+) => {
+  console.log("TRY MERGE");
+
+  const paramsFetchMn: ApiParams = {
+    action: "wbgetentities",
+    sites: "mnwiki",
+    titles: mnArticle,
+  };
+
+  const paramsFetchEn: ApiParams = {
+    action: "wbgetentities",
+    sites: "enwiki",
+    titles: enArticle,
+  };
+
+  const result = (await bot
+    .query(paramsFetchMn)
+    .catch((e: any) => console.log("Error occured: ", e))) as WikibaseEntity;
+  const keys = Object.keys(result?.entities);
+
+  if (keys.length === 1) {
+    console.log("Can merge");
+    const mnWikidataId = keys[0];
+
+    const enResult = (await bot
+      .query(paramsFetchEn)
+      .catch((e: any) => console.log("Error occured: ", e))) as WikibaseEntity;
+    const enWikidataId = Object.keys(enResult?.entities)[0];
+
+    return { en: enWikidataId, mn: mnWikidataId };
+  }
+  console.log(keys);
+  return false;
+};
+
 const linkToWikidata = async () => {
   const bot = new mwn(WikidataBotConfig());
   await bot.login();
@@ -12,8 +62,6 @@ const linkToWikidata = async () => {
   });
 
   while (true) {
-    await bot.getTokens();
-
     const mnArticle: string = await new Promise((resolve) => {
       rl.question("Add an mn article name: ", resolve);
     });
@@ -38,6 +86,7 @@ const linkToWikidata = async () => {
 
     console.log(`Processing: ${mnArticle} to ${enArticle}`);
 
+    await bot.getTokens();
     const token = await bot.getCsrfToken();
 
     const params: ApiParams = {
@@ -49,11 +98,23 @@ const linkToWikidata = async () => {
       token,
     };
 
-    const result = await bot
-      .query(params)
-      .catch((e) => console.log("Error occured: ", e));
+    const result = await bot.query(params).catch(async (e: any) => {
+      console.log("Error occured, try merging: ", e);
+      const wikiDataIds = await getWikidataIds(bot, mnArticle, enArticle);
+      if (wikiDataIds) {
+        const mergeParams: ApiParams = {
+          action: "wbmergeitems",
+          fromid: wikiDataIds.mn,
+          toid: wikiDataIds.en,
+          token: token,
+        };
 
-    console.log(result);
+        const nextResult = await bot.query(mergeParams);
+        console.log(nextResult);
+      } else {
+        console.log("Not mergeable");
+      }
+    });
 
     await new Promise((r) => setTimeout(r, 1500));
   }
