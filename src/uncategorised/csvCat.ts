87f0a24680405ef0b118c67096eb6eb3bba48018
Chintogtokh Batbold <bchintogtokh@gmail.com>
2023-01-24 17:32:45 +1100

    csvCat

---
+++ src/uncategorised/csvCat.ts
@@ -0,0 +1,42 @@
+import { mwn } from "mwn";
+import * as fs from "fs";
+import { BotConfig } from "../utils/bot";
+
+const FILE = "/Users/chintogtokh/dev/UlaanbaatarBot/src/uncategorised/1.csv";
+
+const main = async () => {
+  const bot = new mwn(BotConfig);
+  await bot.login();
+
+  const parseCSV = () => {
+    const articles: string[] = [];
+    const arr = fs.readFileSync(FILE).toString().split("\n");
+    articles.push(...arr.slice(1));
+    return articles;
+  };
+
+  const articles = parseCSV();
+
+  for await (const article of articles) {
+    const splitted = article.replace("\r", "").split(",");
+    const name = splitted[0];
+    const categoryNames = splitted.slice(1).filter((v) => v);
+    console.log(name, categoryNames);
+    // await bot.edit(name, (rev) => {
+    //   let text =
+    //     rev.content +
+    //     "\n" +
+    //     categoryNames.map((v) => `[[Ангилал:${v}]]`).join("\n");
+    //   console.log(text);
+    //   return {
+    //     text: text,
+    //     summary: "Анги нэмэв",
+    //     minor: true,
+    //   };
+    // });
+
+    await new Promise((r) => setTimeout(r, 2000));
+  }
+};
+
+main();
