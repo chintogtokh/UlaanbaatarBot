ca605938c6db74c74036111ba1559cbb0113d5da
Chintogtokh Batbold <bchintogtokh@gmail.com>
2023-05-03 00:28:07 +1000

    wipwipwip

--- src/projects/createDateArticles/createDateArticles.ts
+++ src/projects/createDateArticles/createDateArticles.ts
@@ -1,5 +1,59 @@
-import { mwn } from "mwn";
+import { ApiParams, mwn } from "mwn";
 import * as dotenv from "dotenv";
+import { WikidataBotConfig } from "../../utils/bot";
+// import { getWikidataIds } from "../linkToWikidata/linkToWikidataInteractive";
+
+//copied
+
+type WikibaseEntity = {
+  entities: {
+    [key: string]: {
+      pageid: number;
+      title: string;
+      sitelinks: any;
+    };
+  };
+  success: boolean;
+};
+
+export const getWikidataIds = async (
+  bot: mwn,
+  mnArticle: string,
+  enArticle: string
+) => {
+  console.log("TRY MERGE");
+
+  const paramsFetchMn: ApiParams = {
+    action: "wbgetentities",
+    sites: "mnwiki",
+    titles: mnArticle,
+  };
+
+  const paramsFetchEn: ApiParams = {
+    action: "wbgetentities",
+    sites: "enwiki",
+    titles: enArticle,
+  };
+
+  const result = (await bot
+    .query(paramsFetchMn)
+    .catch((e: any) => console.log("Error occured: ", e))) as WikibaseEntity;
+  const keys = Object.keys(result?.entities);
+
+  if (keys.length === 1) {
+    console.log("Can merge");
+    const mnWikidataId = keys[0];
+
+    const enResult = (await bot
+      .query(paramsFetchEn)
+      .catch((e: any) => console.log("Error occured: ", e))) as WikibaseEntity;
+    const enWikidataId = Object.keys(enResult?.entities)[0];
+
+    return { en: enWikidataId, mn: mnWikidataId };
+  }
+  console.log(keys);
+  return false;
+};
 
 dotenv.config();
 
@@ -15,10 +69,13 @@ enum ArticleType {
   Year = "Year",
   DiedIn = "DiedIn",
   BornIn = "BornIn",
+  YearEstablished = "YearEstablished",
 }
 
 const categoryName = (year: number, type: string) => {
   switch (type) {
+    case ArticleType.YearEstablished:
+      return `Ангилал:${year} онд байгуулагдсан`;
     case ArticleType.Year:
       return `Ангилал:${year} он`;
     case ArticleType.DiedIn:
@@ -32,6 +89,11 @@ const categoryName = (year: number, type: string) => {
 
 const categoryContent = (year: number, type: string) => {
   switch (type) {
+    case ArticleType.YearEstablished:
+      return `{{Commonscat|${year} establishments|${year} онд байгуулагдсан}}
+      {{Байгуулагдсан он|${year}}}
+      [[Ангилал:${Math.floor(year / 10) * 10}-д онд байгуулагдсан|#]]
+      `;
     case ArticleType.Year:
       return `{{Commonscat|${year}}}\n[[Ангилал:${year
         .toString()
@@ -63,15 +125,19 @@ const createDateArticles = async () => {
   const bot = new mwn(config);
   await bot.login();
 
+  const wikidatabot = new mwn(WikidataBotConfig());
+  await wikidatabot.login();
+
   const yearsToAdd = {
     [ArticleType.BornIn]: [],
-    [ArticleType.DiedIn]: [],
-    [ArticleType.Year]: [1615, 1624, 1692, 46, 685],
+    [ArticleType.DiedIn]: [1321, 1681, 1701, 1725, 1728, 1771],
+    [ArticleType.Year]: [],
+    [ArticleType.YearEstablished]: [],
   };
 
   for (const articleType of Object.keys(ArticleType)) {
     for await (const year of yearsToAdd[articleType as ArticleType]) {
-      const summary = "Автоматаар үүсгэж байна";
+      const summary = "Үүсгэж байна";
       const name = categoryName(year, articleType);
       const content = categoryContent(year, articleType);
 
@@ -79,6 +145,45 @@ const createDateArticles = async () => {
       console.log(content);
 
       await bot.create(name!, content!, summary);
+      await new Promise((r) => setTimeout(r, 2000));
+
+      //wikidata
+      await wikidatabot.getTokens();
+
+      const token = await wikidatabot.getCsrfToken();
+
+      //BEWARE
+
+      const mnArticle = `Ангилал:${year} онд өнгөрсөн`;
+      const enArticle = `Category:${year} deaths`;
+
+      const params: ApiParams = {
+        action: "wblinktitles",
+        fromsite: "mnwiki",
+        fromtitle: mnArticle,
+        tosite: "enwiki",
+        totitle: enArticle,
+        token,
+      };
+
+      const result = await wikidatabot.query(params).catch(async (e: any) => {
+        console.log("Error occured, try merging: ", e);
+        const wikiDataIds = await getWikidataIds(bot, mnArticle, enArticle);
+        if (wikiDataIds) {
+          const mergeParams: ApiParams = {
+            action: "wbmergeitems",
+            fromid: wikiDataIds.mn,
+            toid: wikiDataIds.en,
+            token: token,
+          };
+
+          const nextResult = await wikidatabot.query(mergeParams);
+          console.log(nextResult);
+        } else {
+          console.log("Not mergeable");
+        }
+      });
+      //wikidataend
       await new Promise((r) => setTimeout(r, 2000));
     }
   }
